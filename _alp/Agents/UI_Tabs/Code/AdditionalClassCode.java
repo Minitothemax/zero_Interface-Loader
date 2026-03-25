521cbea7f9f8c2f077aad9d117af823e2caf4774
Minitothemax <50232899+Minitothemax@users.noreply.github.com>
2026-03-25 15:42:09 +0100

    Getters voor knoppen toegevoegd

--- _alp/Agents/UI_Tabs/Code/AdditionalClassCode.java
+++ _alp/Agents/UI_Tabs/Code/AdditionalClassCode.java
@@ -7,3 +7,37 @@ public ShapeEmbeddedObjectPresentation getElectricityPresentation() {
 	return pop_tabElectricity_presentation;
 }
 */
+
+//Changing the placement of the buttons for different configurations
+//Button group
+public ShapeGroup getGr_energyDemandSettings(){
+	return gr_energyDemandSettings;
+}
+
+// Mobility button
+public ShapeGroup getGr_mobilityButton(){
+	return gr_mobilityButton;
+}
+
+public ShapeText getTxt_mobilityButtonDescription(){
+	return txt_mobilityButtonDescription;
+}
+
+// Electricity button
+public ShapeGroup getGr_electricityButton(){
+ 	return gr_electricityButton;
+}
+
+public ShapeText getTxt_electricityButtonDescription() {
+	return txt_electricityButtonDescription;
+}
+
+
+// eHub button
+public ShapeGroup getGr_eHubButton(){
+	return gr_eHubButton;
+}
+
+public ShapeText getTxt_eHubButtonDescription(){
+	return txt_eHubButtonDescription;
+}
